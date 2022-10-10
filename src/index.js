7f1e3336fcba63d27092dc00d7235392e4a5b66e
EricHerduin <ericherduin@icloud.com>
2022-10-10 22:38:19 +0200

    MAJ des routes et ajout des routes error

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Index from "./pages/index";
 import reportWebVitals from "./reportWebVitals";
 import ErrorPage from "./pages/error-page";
 import About from "./pages/about";
+import Logement from "./pages/logement";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Index/>,
-    errorElement: <ErrorPage/>
+    element: <Index />,
+    errorElement: <ErrorPage />,
   },
   {
-    path: "/about",
-    element: <About/>
-  }
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "logement/:_id",
+    element: <Logement />,
+    errorElement: <ErrorPage />,
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
