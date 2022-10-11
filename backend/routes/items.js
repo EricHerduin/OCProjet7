f3bd5d55b0682752a7bb5dc4e951397db4368775
EricHerduin <ericherduin@icloud.com>
2022-10-11 09:28:58 +0200

    suppression du backend

--- backend/routes/items.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const itemsCtrl = require("../controllers/items");
-
-router.get("/", itemsCtrl.getAllItems);
-router.get("/:_id", itemsCtrl.getOneItem);
-
-module.exports = router;
