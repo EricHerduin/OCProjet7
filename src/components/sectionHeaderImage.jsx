3f1dc9a25ba0d623bc790005f48d4ec891c06a3f
EricHerduin <ericherduin@icloud.com>
2022-10-11 15:00:04 +0200

    modification pour intégration de l'image

--- src/components/sectionHeaderImage.jsx
+++ src/components/sectionHeaderImage.jsx
@@ -2,10 +2,7 @@ import "../CSS/styles.css";
 
 export default function SectionHeaderImg(props) {
   return (
-    <div
-      className={props.class}
-      style={{ backgroundImage: `url(${props.image})` }}
-    >
+    <div className={props.class}>
       {/* <img className="sectionHeaderImg__img" src={Img} alt=""/> */}
       <div className="sectionHeaderImg__darken"></div>
       <p className="sectionHeaderImg__text">{props.text}</p>
